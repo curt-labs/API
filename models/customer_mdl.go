e5b70ac276d2de1643fdbaf624f20b856ab1da34
ninnemana <ninnemana@gmail.com>
2013-03-21 12:53:41 -0500

    removed byte array conversion

--- models/customer_mdl.go
+++ models/customer_mdl.go
@@ -727,7 +727,7 @@ func GetLocalRegions() (regions []StateRegion, err error) {
 			}
 
 			if regions_bytes, err = json.Marshal(regions); err == nil {
-				client.Set("local_regions", []byte(regions_bytes))
+				client.Set("local_regions", regions_bytes)
 				client.Expire("local_regions", int64(time.Duration.Hours(24)))
 			}
 		}
