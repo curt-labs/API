ac521cd2b7182a04e76b6fdcdda9162be3e585e1
Alex Ninneman <alex@ninneman.org>
2014-04-29 11:00:07 -0500

    swapped out redis integration

--- helpers/redis/redis_hlp.go
+++ helpers/redis/redis_hlp.go
@@ -1,7 +1,7 @@
 package redis
 
 import (
-	"log"
+	redisio "github.com/hoisie/redis"
 	"os"
 )
 
@@ -10,12 +10,18 @@ var (
 	RedisMaster = NewRedisMaster()
 )
 
-func NewRedisClient() *Client {
+func NewClient(poolsize int) (c *redisio.Client) {
+	_c := new(redisio.Client)
+	_c.MaxPoolSize = poolsize
+
+	return _c
+}
+
+func NewRedisClient() *redisio.Client {
 	c := NewClient(50)
 
 	c.Addr = "127.0.0.1:6379"
 	if addr := os.Getenv("REDIS_CLIENT_ADDRESS"); addr != "" {
-		log.Println(addr)
 		c.Addr = addr
 	}
 
@@ -25,7 +31,7 @@ func NewRedisClient() *Client {
 	return c
 }
 
-func NewRedisMaster() *Client {
+func NewRedisMaster() *redisio.Client {
 	c := NewClient(50)
 
 	c.Addr = "127.0.0.1:6379"
