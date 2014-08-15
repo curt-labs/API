70d60aa27d748f33db4a5ff07a8c11881a45cb56
Alex Ninneman <alex@ninneman.org>
2014-08-15 15:17:27 -0500

    fully tested aces models

--- controllers/home.go
+++ controllers/home.go
@@ -1,20 +1,10 @@
 package controllers
 
 import (
-	"github.com/curt-labs/GoAPI/helpers/plate"
-	. "github.com/curt-labs/GoAPI/models"
-
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	var v Vehicle
-
-	config := ConfigResponse{
-		ConfigOption: v.GetYears(),
-		Matched:      new(ProductMatch),
-	}
-
-	plate.ServeFormatted(w, r, config)
+	http.Redirect(w, r, "http://labs.curtmfg.com", http.StatusFound)
 	return
 }
