264571d31da4ae17b6d76338b81ec1e912f9fde7
John Shenk <john_shenk@hotmail.com>
2014-09-03 09:15:18 -0500

    Makes Faq test use a random faq; Creates/Updates/Deletes the same test object

--- models/faq/faq_model_test.go
+++ models/faq/faq_model_test.go
@@ -2,19 +2,13 @@ package faq_model
 
 import (
 	. "github.com/smartystreets/goconvey/convey"
+	"math/rand"
+	"strconv"
 	"testing"
 )
 
 func TestGetFaqs(t *testing.T) {
 	Convey("Testing Gets", t, func() {
-		Convey("Testing Get()", func() {
-			var f Faq
-			f.ID = 1
-			f.Get()
-			So(f, ShouldNotBeNil)
-			So(f.Question, ShouldEqual, "Can you use weight distribution on trailers with surge brakes?")
-			So(f.Answer, ShouldEqual, "Yes. The coupler still has solid contact to the ball with a free range of motion.")
-		})
 		Convey("Testing GetAll()", func() {
 			var fs Faqs
 			fs, err := GetAll()
@@ -22,35 +16,55 @@ func TestGetFaqs(t *testing.T) {
 			So(err, ShouldBeNil)
 			So(len(fs), ShouldNotBeNil)
 		})
-		Convey("Testing GetQuestions()", func() {
-			qs, err := GetQuestions("1", "3")
-			So(qs, ShouldNotBeNil)
+		Convey("Gets a faqs and a random faq, page, resultsperpage", func() {
+			fs, err := GetAll()
 			So(err, ShouldBeNil)
-			So(qs.Pagination.Page, ShouldEqual, 1)
-			So(qs.Pagination.ReturnedCount, ShouldNotBeNil)
-			So(qs.Pagination.PerPage, ShouldEqual, len(qs.Objects))
-		})
-		Convey("Testing GetAnswers()", func() {
-			as, err := GetAnswers("1", "0")
-			So(as, ShouldNotBeNil)
-			So(err, ShouldBeNil)
-			So(as.Pagination.Page, ShouldEqual, 1)
-			So(as.Pagination.ReturnedCount, ShouldNotBeNil)
-			So(as.Pagination.PerPage, ShouldNotBeNil)
-			So(as.Pagination.PerPage, ShouldEqual, len(as.Objects))
-		})
-		Convey("Testing Search()", func() {
-			as, err := Search("hitch", "", "1", "0")
-			So(as, ShouldNotBeNil)
-			So(err, ShouldBeNil)
-			So(as.Pagination.Page, ShouldEqual, 1)
-			So(as.Pagination.ReturnedCount, ShouldNotBeNil)
-			So(as.Pagination.PerPage, ShouldNotBeNil)
-			So(as.Pagination.PerPage, ShouldEqual, len(as.Objects))
+			if len(fs) > 0 {
+				x := rand.Intn(len(fs))
+				f := fs[x]
+				totalResults := len(fs)
+				result := strconv.Itoa(totalResults / 3)
+				page := "3"
+
+				Convey("Testing Get()", func() {
+					f.Get()
+					So(f, ShouldNotBeNil)
+					So(f.Question, ShouldHaveSameTypeAs, "str")
+					So(f.Answer, ShouldHaveSameTypeAs, "str")
+				})
+
+				Convey("Testing GetQuestions()", func() {
+
+					qs, err := GetQuestions(page, result)
+					So(qs, ShouldNotBeNil)
+					So(err, ShouldBeNil)
+					So(qs.Pagination.Page, ShouldEqual, 3)
+					So(qs.Pagination.ReturnedCount, ShouldNotBeNil)
+					So(qs.Pagination.PerPage, ShouldEqual, len(qs.Objects))
+				})
+				Convey("Testing GetAnswers()", func() {
+					as, err := GetAnswers(page, result)
+					So(as, ShouldNotBeNil)
+					So(err, ShouldBeNil)
+					So(as.Pagination.Page, ShouldEqual, 3)
+					So(as.Pagination.ReturnedCount, ShouldNotBeNil)
+					So(as.Pagination.PerPage, ShouldNotBeNil)
+					So(as.Pagination.PerPage, ShouldEqual, len(as.Objects))
+				})
+				Convey("Testing Search()", func() {
+					as, err := Search(f.Question, "", "1", "0")
+					So(as, ShouldNotBeNil)
+					So(err, ShouldBeNil)
+					So(as.Pagination.Page, ShouldEqual, 1)
+					So(as.Pagination.ReturnedCount, ShouldNotBeNil)
+					So(as.Pagination.PerPage, ShouldNotBeNil)
+					So(as.Pagination.PerPage, ShouldEqual, len(as.Objects))
+				})
+			}
 		})
 
 	})
-	Convey("Testing CUD", t, func() {
+	Convey("Testing C_UD", t, func() {
 		Convey("Testing Create()", func() {
 			var f Faq
 			f.Question = "testQuestion"
@@ -59,24 +73,21 @@ func TestGetFaqs(t *testing.T) {
 			So(f, ShouldNotBeNil)
 			So(f.Question, ShouldEqual, "testQuestion")
 			So(f.Answer, ShouldEqual, "testAnswer")
-		})
-		Convey("Testing Update()", func() {
-			var f Faq
-			f.ID = 15
-			f.Question = "testQuestion222"
-			f.Answer = "testAnswer222"
-			f.Update()
-			So(f, ShouldNotBeNil)
-			So(f.Question, ShouldEqual, "testQuestion222")
-			So(f.Answer, ShouldEqual, "testAnswer222")
-		})
-		Convey("Testing Delete()", func() {
-			var f Faq
-			f.ID = 15
-			f.Delete()
-			f.Get()
-			So(f.Question, ShouldBeBlank)
-			So(f.Answer, ShouldBeBlank)
+
+			Convey("Testing Update()", func() {
+				f.Question = "testQuestion222"
+				f.Answer = "testAnswer222"
+				f.Update()
+				So(f, ShouldNotBeNil)
+				So(f.Question, ShouldEqual, "testQuestion222")
+				So(f.Answer, ShouldEqual, "testAnswer222")
+
+				Convey("Testing Delete()", func() {
+					f.Get()
+					err := f.Delete()
+					So(err, ShouldBeNil)
+				})
+			})
 		})
 
 	})
