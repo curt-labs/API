5cfef05478f4f550ba910a5dac54e1efcd7221f7
John Shenk <john_shenk@hotmail.com>
2014-10-31 10:41:02 -0500

    News test passes empty DB:

--- models/news/news_model_test.go
+++ models/news/news_model_test.go
@@ -7,18 +7,20 @@ import (
 )
 
 func TestNews(t *testing.T) {
+	var n News
+	var err error
+	n.Title = "test news"
 
-	Convey("Test CRUD", t, func() {
-		var n News
-		var err error
-		n.Title = "test news"
+	Convey("Test Create", t, func() {
 		err = n.Create()
 		So(err, ShouldBeNil)
-
+	})
+	Convey("Test Update", t, func() {
 		n.Title = "Different Title"
 		err = n.Update()
 		So(err, ShouldBeNil)
-
+	})
+	Convey("Test Get", t, func() {
 		err = n.Get()
 		So(err, ShouldBeNil)
 		So(n.Title, ShouldEqual, "Different Title")
@@ -27,11 +29,6 @@ func TestNews(t *testing.T) {
 		So(len(obj.Objects), ShouldEqual, 1)
 		So(err, ShouldBeNil)
 
-		err = n.Delete()
-		So(err, ShouldBeNil)
-	})
-	Convey("Test Get-Alls", t, func() {
-		var err error
 		ns, err := GetAll()
 		if err != sql.ErrNoRows {
 			So(err, ShouldBeNil)
@@ -49,4 +46,27 @@ func TestNews(t *testing.T) {
 		}
 
 	})
+	Convey("Test Delete", t, func() {
+		err = n.Delete()
+		So(err, ShouldBeNil)
+	})
+	// Convey("Test Get-Alls", t, func() {
+	// 	var err error
+	// 	ns, err := GetAll()
+	// 	if err != sql.ErrNoRows {
+	// 		So(err, ShouldBeNil)
+	// 		So(len(ns), ShouldBeGreaterThan, 0)
+	// 	}
+	// 	ts, err := GetTitles("1", "1")
+	// 	if err != sql.ErrNoRows {
+	// 		So(err, ShouldBeNil)
+	// 		So(len(ts.Objects), ShouldBeGreaterThan, 0)
+	// 	}
+	// 	ls, err := GetLeads("1", "1")
+	// 	if err != sql.ErrNoRows {
+	// 		So(err, ShouldBeNil)
+	// 		So(len(ls.Objects), ShouldBeGreaterThan, 0)
+	// 	}
+
+	// })
 }
