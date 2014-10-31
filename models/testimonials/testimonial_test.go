33825f46c6955a736a67b3eb0757bd21ce104256
John Shenk <john_shenk@hotmail.com>
2014-10-31 13:51:10 -0500

    techSupport passes emptyDB

--- models/testimonials/testimonial_test.go
+++ models/testimonials/testimonial_test.go
@@ -7,49 +7,52 @@ import (
 )
 
 func TestTestimonials(t *testing.T) {
-	Convey("Testing CRUD", t, func() {
-		var test Testimonial
-		var err error
+	var test Testimonial
+	var err error
+	Convey("Testing Create Testimonial", t, func() {
 		test.Content = "Test Content"
 		err = test.Create()
 		So(err, ShouldBeNil)
-
+	})
+	Convey("Update", t, func() {
 		test.Content = "New Content"
 		err = test.Update()
 		So(err, ShouldBeNil)
 
+	})
+
+	Convey("GetAll - No paging", t, func() {
 		err = test.Get()
 		So(err, ShouldBeNil)
+	})
+	Convey("GetAll - No paging", t, func() {
+		ts, err := GetAllTestimonials(0, 0, false)
+		if err != sql.ErrNoRows {
+			So(err, ShouldBeNil)
+			So(len(ts), ShouldBeGreaterThan, 0)
+		}
+	})
 
-		err = test.Delete()
-		So(err, ShouldBeNil)
+	Convey("GetAll - Paged", t, func() {
+		ts, err := GetAllTestimonials(1, 1, false)
+		if err != sql.ErrNoRows {
+			So(err, ShouldBeNil)
+			So(len(ts), ShouldBeGreaterThan, 0)
+		}
+	})
+
+	Convey("GetAll - randomized", t, func() {
+		ts, err := GetAllTestimonials(1, 1, true)
+		if err != sql.ErrNoRows {
+			So(err, ShouldBeNil)
+			So(len(ts), ShouldBeGreaterThan, 0)
+		}
 
 	})
-	Convey("Testing GetAll", t, func() {
-		var ts Testimonials
-		var err error
-		Convey("GetAll - No paging", func() {
-			ts, err = GetAllTestimonials(0, 0, false)
-			if err != sql.ErrNoRows {
-				So(err, ShouldBeNil)
-				So(len(ts), ShouldBeGreaterThan, 0)
-			}
-		})
+	// Convey("Delete", t, func() {
+	// 	err = test.Delete()
+	// 	So(err, ShouldBeNil)
 
-		Convey("GetAll - Paged", func() {
-			ts, err = GetAllTestimonials(1, 20, false)
-			if err != sql.ErrNoRows {
-				So(err, ShouldBeNil)
-				So(len(ts), ShouldBeGreaterThan, 0)
-			}
-		})
+	// })
 
-		Convey("GetAll - randomized", func() {
-			ts, err = GetAllTestimonials(1, 2, true)
-			if err != sql.ErrNoRows {
-				So(err, ShouldBeNil)
-				So(len(ts), ShouldBeGreaterThan, 0)
-			}
-		})
-	})
 }
