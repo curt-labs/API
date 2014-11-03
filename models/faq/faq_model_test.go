7b5f5361677ca3910d63fea6f41bf105db57e33a
John Shenk <john_shenk@hotmail.com>
2014-11-03 15:34:28 -0600

    Gets rid of dumb faq methods

--- models/faq/faq_model_test.go
+++ models/faq/faq_model_test.go
@@ -33,31 +33,13 @@ func TestGetFaqs(t *testing.T) {
 		So(f.Question, ShouldHaveSameTypeAs, "str")
 		So(f.Answer, ShouldHaveSameTypeAs, "str")
 
-		page := "1"
-		result := "1"
-
-		qs, err := GetQuestions(page, result)
-		So(qs, ShouldNotBeNil)
-		So(err, ShouldBeNil)
-		So(qs.Pagination.Page, ShouldEqual, 1)
-		So(qs.Pagination.ReturnedCount, ShouldNotBeNil)
-		So(qs.Pagination.PerPage, ShouldEqual, len(qs.Objects))
-
-		as, err := GetAnswers(page, result)
-		So(as, ShouldNotBeNil)
-		So(err, ShouldBeNil)
-		So(as.Pagination.Page, ShouldEqual, 1)
-		So(as.Pagination.ReturnedCount, ShouldNotBeNil)
-		So(as.Pagination.PerPage, ShouldNotBeNil)
-		So(as.Pagination.PerPage, ShouldEqual, len(as.Objects))
-
 		var fs Faqs
 		fs, err = GetAll()
 		So(fs, ShouldNotBeNil)
 		So(err, ShouldBeNil)
 		So(len(fs), ShouldNotBeNil)
 
-		as, err = Search(f.Question, "", "1", "0")
+		as, err := Search(f.Question, "", "1", "0")
 		So(as, ShouldNotBeNil)
 		So(err, ShouldBeNil)
 		So(as.Pagination.Page, ShouldEqual, 1)
