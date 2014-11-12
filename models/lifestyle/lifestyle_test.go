17c5d962329bfb04539b9cc5a1a1a6328717720b
John Shenk <john_shenk@hotmail.com>
2014-11-12 15:43:05 -0600

    nests lifetsyle tests

--- models/lifestyle/lifestyle_test.go
+++ models/lifestyle/lifestyle_test.go
@@ -20,41 +20,39 @@ func TestGetLifestyles(t *testing.T) {
 			So(l, ShouldNotBeNil)
 			So(l.Name, ShouldEqual, "testName")
 			So(l.LongDesc, ShouldEqual, "Long description")
-		})
 
-		Convey("Testing Update()", func() {
-			l.Name = "newName"
-			l.Image = "image"
-			l.ShortDesc = "Desc"
-			err := l.Update()
-			So(err, ShouldBeNil)
-			err = l.Get()
-			t.Log(l)
-			So(err, ShouldBeNil)
-			So(l, ShouldNotBeNil)
-			So(l.Name, ShouldEqual, "newName")
-			So(l.Image, ShouldEqual, "image")
-			So(l.ShortDesc, ShouldEqual, "Desc")
-		})
+			Convey("Testing Update()", func() {
+				l.Name = "newName"
+				l.Image = "image"
+				l.ShortDesc = "Desc"
+				err := l.Update()
+				So(err, ShouldBeNil)
+				err = l.Get()
+				t.Log(l)
+				So(err, ShouldBeNil)
+				So(l, ShouldNotBeNil)
+				So(l.Name, ShouldEqual, "newName")
+				So(l.Image, ShouldEqual, "image")
+				So(l.ShortDesc, ShouldEqual, "Desc")
 
-		Convey("Test Gets", func() {
-			ls, err := GetAll()
-			So(err, ShouldBeNil)
-			So(len(ls), ShouldBeGreaterThan, 0)
+				Convey("Test Gets", func() {
+					ls, err := GetAll()
+					So(err, ShouldBeNil)
+					So(len(ls), ShouldBeGreaterThan, 0)
 
-			err = l.Get()
-			So(err, ShouldBeNil)
-			So(l, ShouldNotBeNil)
-			So(l.Name, ShouldHaveSameTypeAs, "str")
-			So(l.ShortDesc, ShouldHaveSameTypeAs, "str")
+					err = l.Get()
+					So(err, ShouldBeNil)
+					So(l, ShouldNotBeNil)
+					So(l.Name, ShouldHaveSameTypeAs, "str")
+					So(l.ShortDesc, ShouldHaveSameTypeAs, "str")
 
-		})
+					Convey("Testing Delete()", func() {
+						err := l.Delete()
+						So(err, ShouldBeNil)
 
-		Convey("Testing Delete()", func() {
-			l.Get()
-			err := l.Delete()
-			So(err, ShouldBeNil)
-
+					})
+				})
+			})
 		})
 
 	})
