c85217512b9a3aa9890c1826672702c92cf96474
John Shenk <john_shenk@hotmail.com>
2014-11-12 15:54:21 -0600

    makes lifestyle tests inline

--- models/lifestyle/lifestyle_test.go
+++ models/lifestyle/lifestyle_test.go
@@ -9,51 +9,45 @@ func TestGetLifestyles(t *testing.T) {
 	var l Lifestyle
 
 	Convey("Testing CRUD", t, func() {
-		Convey("Testing Create()", func() {
 
-			l.Name = "testName"
-			l.LongDesc = "Long description"
-			err := l.Create()
-			So(err, ShouldBeNil)
-			err = l.Get()
-			So(err, ShouldBeNil)
-			So(l, ShouldNotBeNil)
-			So(l.Name, ShouldEqual, "testName")
-			So(l.LongDesc, ShouldEqual, "Long description")
+		l.Name = "testName"
+		l.LongDesc = "Long description"
+		err := l.Create()
+		So(err, ShouldBeNil)
+		err = l.Get()
+		So(err, ShouldBeNil)
+		So(l, ShouldNotBeNil)
+		So(l.Name, ShouldEqual, "testName")
+		So(l.LongDesc, ShouldEqual, "Long description")
 
-			Convey("Testing Update()", func() {
-				l.Name = "newName"
-				l.Image = "image"
-				l.ShortDesc = "Desc"
-				err := l.Update()
-				So(err, ShouldBeNil)
-				err = l.Get()
-				t.Log(l)
-				So(err, ShouldBeNil)
-				So(l, ShouldNotBeNil)
-				So(l.Name, ShouldEqual, "newName")
-				So(l.Image, ShouldEqual, "image")
-				So(l.ShortDesc, ShouldEqual, "Desc")
+		//Update
+		l.Name = "newName"
+		l.Image = "image"
+		l.ShortDesc = "Desc"
+		err = l.Update()
+		So(err, ShouldBeNil)
+		err = l.Get()
+		t.Log(l)
+		So(err, ShouldBeNil)
+		So(l, ShouldNotBeNil)
+		So(l.Name, ShouldEqual, "newName")
+		So(l.Image, ShouldEqual, "image")
+		So(l.ShortDesc, ShouldEqual, "Desc")
 
-				Convey("Test Gets", func() {
-					ls, err := GetAll()
-					So(err, ShouldBeNil)
-					So(len(ls), ShouldBeGreaterThan, 0)
+		//Gets
+		ls, err := GetAll()
+		So(err, ShouldBeNil)
+		So(len(ls), ShouldBeGreaterThan, 0)
 
-					err = l.Get()
-					So(err, ShouldBeNil)
-					So(l, ShouldNotBeNil)
-					So(l.Name, ShouldHaveSameTypeAs, "str")
-					So(l.ShortDesc, ShouldHaveSameTypeAs, "str")
+		err = l.Get()
+		So(err, ShouldBeNil)
+		So(l, ShouldNotBeNil)
+		So(l.Name, ShouldHaveSameTypeAs, "str")
+		So(l.ShortDesc, ShouldHaveSameTypeAs, "str")
 
-					Convey("Testing Delete()", func() {
-						err := l.Delete()
-						So(err, ShouldBeNil)
-
-					})
-				})
-			})
-		})
+		//Delete
+		err = l.Delete()
+		So(err, ShouldBeNil)
 
 	})
 }
