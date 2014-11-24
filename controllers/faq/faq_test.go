a32cfc39741f637f6299efaca437af324ca9e0a0
John Shenk <john_shenk@hotmail.com>
2014-11-24 12:10:47 -0600

    adds testrunner to applicationGuide controller; moves Request Benchmark method to testThatHttp package & adds apiKey parameter to url

--- controllers/faq/faq_test.go
+++ controllers/faq/faq_test.go
@@ -8,12 +8,9 @@ package faq_controller
 
 import (
 	"encoding/json"
-	"github.com/curt-labs/GoAPI/helpers/httprunner"
 	"github.com/curt-labs/GoAPI/helpers/pagination"
 	"github.com/curt-labs/GoAPI/helpers/testThatHttp"
-	"github.com/curt-labs/GoAPI/models/customer_new"
 	"github.com/curt-labs/GoAPI/models/faq"
-	"github.com/go-martini/martini"
 	. "github.com/smartystreets/goconvey/convey"
 	"net/url"
 	"strconv"
@@ -73,39 +70,7 @@ func TestFaqs(t *testing.T) {
 }
 
 func BenchmarkGetFaqs(b *testing.B) {
-	//no faq object creation (fails on empty db)
-	var cu customer_new.CustomerUser
-	cu.Name = "test cust user"
-	cu.Email = "pretend@test.com"
-	cu.Password = "test"
-	cu.Sudo = true
-	cu.Create()
-	var apiKey string
-	for _, key := range cu.Keys {
-		if strings.ToLower(key.Type) == "public" {
-			apiKey = key.Key
-		}
-	}
 
-	RequestBenchmark(b.N, "GET", "/faqs/1?key="+apiKey, nil, Get)
-	RequestBenchmark(b.N, "GET", "/faqs?key="+apiKey, nil, GetAll)
-
-	cu.Delete()
-}
-
-func RequestBenchmark(runs int, method, route string, body *url.Values, handler martini.Handler) {
-
-	opts := httprunner.ReqOpts{
-		Body:    body,
-		Handler: handler,
-		URL:     route,
-		Method:  method,
-	}
-
-	(&httprunner.Runner{
-		Req: &opts,
-		N:   runs,
-		C:   1,
-	}).Run()
-
+	testThatHttp.RequestBenchmark(b.N, "GET", "/faqs/1", nil, Get)
+	testThatHttp.RequestBenchmark(b.N, "GET", "/faqs", nil, GetAll)
 }
