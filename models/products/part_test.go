1afa12832e50c4b352d3124d5796fb003f91fc13
stinkyfingers <john_shenk@hotmail.com>
2015-01-07 10:53:58 -0600

    adds brand-specifity to parts.latest and parts.featured; updates model tests accordingly

--- models/products/part_test.go
+++ models/products/part_test.go
@@ -1,14 +1,27 @@
 package products
 
 import (
+	"github.com/curt-labs/GoAPI/helpers/apicontext"
+	"github.com/curt-labs/GoAPI/helpers/apicontextmock"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
-func TestLatest(t *testing.T) {
+func TestPart(t *testing.T) {
+	MockedDTX := &apicontext.DataContext{}
+	var err error
+	if MockedDTX, err = apicontextmock.Mock(); err != nil {
+		return
+	}
 	Convey("Testing GetLatest", t, func() {
-		parts, err := Latest(generateAPIkey(), 10)
+		parts, err := Latest(10, MockedDTX)
 		So(err, ShouldBeNil)
 		So(parts, ShouldHaveSameTypeAs, []Part{})
 	})
+	Convey("Testing Featured", t, func() {
+		parts, err := Featured(3, MockedDTX)
+		So(err, ShouldBeNil)
+		So(parts, ShouldHaveSameTypeAs, []Part{})
+	})
+	_ = apicontextmock.DeMock(MockedDTX)
 }
