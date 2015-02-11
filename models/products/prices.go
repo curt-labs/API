7c934e9b51a3d0b9e8e7e32cef5fa3580109d91e
stinkyfingers <john_shenk@hotmail.com>
2015-02-11 17:10:58 -0600

    includes price field in products/price, even when empty

--- models/products/prices.go
+++ models/products/prices.go
@@ -15,7 +15,7 @@ type Price struct {
 	Id           int       `json:"id,omitempty" xml:"id,omitempty"`
 	PartId       int       `json:"partId,omitempty" xml:"partId,omitempty"`
 	Type         string    `json:"type,omitempty" xml:"type,omitempty"`
-	Price        float64   `json:"price,omitempty" xml:"price,omitempty"`
+	Price        float64   `json:"price" xml:"price"`
 	Enforced     bool      `json:"enforced,omitempty", xml:"enforced, omitempty"`
 	DateModified time.Time `json:"dateModified,omitempty" xml:"dateModified,omitempty"`
 }
