ba9b0768ccbd39574e446f3ca4c4438f77ea3e42
Alex Ninneman <alex@ninneman.org>
2015-10-10 20:52:26 -0500

    changed DB ref

--- models/products/mongo_layer.go
+++ models/products/mongo_layer.go
@@ -9,18 +9,19 @@ import (
 func GetCategoryTree() ([]Category, error) {
 	var cats []Category
 
-	session, err := mgo.DialWithInfo(database.MongoCategoryConnectionString())
+	session, err := mgo.DialWithInfo(database.MongoPartConnectionString())
 	if err != nil {
 		return cats, err
 	}
 	defer session.Close()
 	query := bson.M{"parent_id": 0}
-	err = session.DB(database.CategoryDatabase).C(database.CategoryCollectionName).Find(query).All(&cats)
+	err = session.DB(database.ProductDatabase).C(database.CategoryCollectionName).Find(query).All(&cats)
 	return cats, err
 }
 
 func GetCategoryParts(catId int) ([]Part, error) {
 	var parts []Part
+
 	session, err := mgo.DialWithInfo(database.MongoPartConnectionString())
 	if err != nil {
 		return parts, err
