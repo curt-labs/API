6e976691bb0bec6cd283219092b2a18e6210a901
Alex Ninneman <alex@ninneman.org>
2015-10-29 14:30:28 -0500

    renamed attribute key struct tags

--- models/products/attributes.go
+++ models/products/attributes.go
@@ -1,7 +1,7 @@
 package products
 
 type Attribute struct {
-	Key   string `json:"key" xml:"key,attr" bson:"key"`
+	Key   string `json:"name" xml:"name,attr" bson:"name"`
 	Value string `json:"value" xml:",chardata" bson:"value"`
 	Sort  int    `json:"sort,omitempty" xml:"sort,omitempty" bson:"sort"`
 }
