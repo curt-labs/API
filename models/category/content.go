627f8e0a5b09ba8a07967e17e89c4b6dddbbeb84
davidvaini <davidvaini@gmail.com>
2015-12-08 11:04:45 -0600

    added missing fields to category content

--- models/category/content.go
+++ models/category/content.go
@@ -2,13 +2,15 @@ package category
 
 // Content ...
 type Content struct {
+	Id          int         `bson:"contentid" json:"id" xml:"id"`
 	Text        string      `bson:"text" json:"text" xml:"text"`
 	ContentType ContentType `json:"contentType" xml:"contentType"`
 }
 
 // ContentType ...
 type ContentType struct {
-	Id         int    `json:"id" xml:"id"`
+	Id         int    `bson:"contenttypeid" json:"id" xml:"id"`
 	Type       string `bson:"type" json:"type" xml:"type"`
 	AllowsHTML bool   `bson:"allows_html" json:"allows_html" xml:"allows_html"`
+	IsPrivate  bool   `bson:"isprivate" json:"isprivate" xml:"isprivate"`
 }
