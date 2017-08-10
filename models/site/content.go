fab5ca57ece055ef4fa35b5681d584efb551114e
Lehman Black <lblack@curtmfg.com>
2017-08-10 13:56:21 -0500

    Remove API key from /site/content/all (getAllContent)

--- models/site/content.go
+++ models/site/content.go
@@ -52,10 +52,11 @@ var (
 	getLatestRevision = `SELECT revisionID, content_text, createdOn, active FROM SiteContentRevision AS scr WHERE scr.contentID = ? ORDER BY createdOn DESC LIMIT 1`
 	getContent        = `SELECT ` + siteContentColumns + ` FROM SiteContent AS s WHERE s.contentID = ?`
 	getAllContent = `SELECT ` + siteContentColumns + ` FROM SiteContent AS s
-								Join WebsiteToBrand as wub on wub.WebsiteID = s.websiteID
-								Join ApiKeyToBrand as akb on akb.brandID = wub.brandID
-								Join ApiKey as ak on akb.keyID = ak.id
-								where s.active = true && s.published = true && (ak.api_key = ? && (wub.brandID = ? OR 0=?)  && (wub.WebsiteID = ? OR 0=?))`
+		JOIN WebsiteToBrand AS wub ON wub.WebsiteID = s.websiteID
+		WHERE s.active = TRUE &&
+		s.published    = TRUE &&
+		wub.brandID    = ?    &&
+		s.WebsiteID    = ?`
 	getContentRevisions    = `SELECT revisionID, content_text, createdOn, active FROM SiteContentRevision AS scr WHERE scr.contentID = ? `
 	getAllContentRevisions = `SELECT revisionID, content_text, createdOn, active FROM SiteContentRevision AS scr `
 	getContentRevision     = `SELECT revisionID, content_text, createdOn, active FROM SiteContentRevision AS scr WHERE revisionID = ?`
